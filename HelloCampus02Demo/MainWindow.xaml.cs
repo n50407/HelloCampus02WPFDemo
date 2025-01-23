662262c95d86baa94d02d34cc38e08b63231bdfc
Johann Grabner <jgrabner@business-tuning.at>
2025-01-23 11:51:29 +0100

    Add project files.

---
+++ HelloCampus02Demo/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloCampus02Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    class Person
+    {
+        public string Vorname { get; set; }
+    }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hallo Welt");
+            Person p1 = new Person();
+            //p1.Vorname.Add("Hans");
+            //p1.Vorname.Add("Hans");
+
+        }
+
+        private void HelloCampus02Cllick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
