f6265d84dcf9ec1449ec83ad40f99ee8c4f92cbe
Johann Grabner <jgrabner@business-tuning.at>
2025-01-30 14:17:29 +0100

    INotifyPropertyChanged

--- HelloCampus02Demo/Models/Produkt.cs
+++ HelloCampus02Demo/Models/Produkt.cs
@@ -1,45 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HelloCampus02Demo.Models
 {
-    internal class Produkt
+    internal class Produkt: INotifyPropertyChanged
     {
-		private int _ProduktID;
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        private int _ProduktID;
 		public int ProduktID
 		{
 			get { return _ProduktID; }
-			set { _ProduktID = value; }
-		}
-		private string _Bezeichnung;
+			set
+            {
+                _ProduktID = value;
+                InformGUI("ProduktID");
+            }
+        }
 
+        private void InformGUI(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        private string _Bezeichnung;
 		public string Bezeichnung
 		{
 			get { return _Bezeichnung; }
-			set { _Bezeichnung = value; }
+			set { _Bezeichnung = value;
+				InformGUI(nameof(Bezeichnung));
+			}
 		}
 
 		private string _Kategorie;
-
 		public string Kategorie
 		{
 			get { return _Kategorie; }
 			set { _Kategorie = value; }
 		}
 		private bool _AufLager;
-
 		public bool AufLager
 		{
 			get { return _AufLager; }
 			set { _AufLager = value; }
 		}
-		private double _Preis;
-
-		public double Preis
+		private double _Preis;     
+        public double Preis
 		{
 			get { return _Preis; }
 			set { _Preis = value; }
