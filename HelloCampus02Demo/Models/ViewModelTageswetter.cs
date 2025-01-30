9074543e66a7cf4555f25dd25dbd30736f7a9138
Johann Grabner <jgrabner@business-tuning.at>
2025-01-30 09:59:46 +0100

    Fahrenheit

--- HelloCampus02Demo/Models/ViewModelTageswetter.cs
+++ HelloCampus02Demo/Models/ViewModelTageswetter.cs
@@ -9,6 +9,17 @@ namespace HelloCampus02Demo.Models
 {
     internal class ViewModelTageswetter: INotifyPropertyChanged //STRG + .
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Dummy()
+        {
+            PropertyChanged(this, 
+                
+                new PropertyChangedEventArgs("Dummy"));
+
+
+        }
+
         //public string AktuelleBewoelkung;
         public string AktuelleBewoelkung { get; set; }
 
@@ -16,18 +27,27 @@ namespace HelloCampus02Demo.Models
         // public double AktuelleTemperatur { get; set; }
         private double _AktuelleTemperatur;
 
-        public event PropertyChangedEventHandler PropertyChanged;
+
+        //fahren=celsius * 1.8 + 32
+
+        public double AktuelleTemperaturFahrenheit { get; set; }
+
 
         public double AktuelleTemperatur
         {
             get { return _AktuelleTemperatur; }
             set {
+                //value = celsius
                 _AktuelleTemperatur = value; 
+                AktuelleTemperaturFahrenheit = _AktuelleTemperatur * 1.8 + 32;
                 if (PropertyChanged != null)
                 {
                     PropertyChanged(this, 
                         new PropertyChangedEventArgs("AktuelleTemperatur")
                         );
+                    PropertyChanged(this,
+                       new PropertyChangedEventArgs("AktuelleTemperaturFahrenheit")
+                       );
                 }
                 //Inform GUI
             
