3193d8468051f0447339dd9a41a3d374054cd4b1
Johann Grabner <jgrabner@business-tuning.at>
2025-02-06 12:37:47 +0100

    ViewModels

---
+++ HelloCampus02Demo/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,53 @@
+﻿using HelloCampus02Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCampus02Demo.ViewModels
+{
+    internal class ProductListViewModel: INotifyPropertyChanged
+    {
+        //List<Produkt> meineProdukte ...keine ChngeNotification
+
+        //automatische ChangeNotification
+        public ObservableCollection<Produkt> ProduktList { get; set; }
+
+        private int _ProduktAnzahl;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void InformGUI(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        
+
+         public Produkt AusgewaehltesProdukt { get; set; }
+
+        public int ProduktAnzahl
+        {
+            get { return _ProduktAnzahl; }
+            set {
+                _ProduktAnzahl = value; 
+                InformGUI(nameof(ProduktAnzahl));
+            
+            }
+        }
+
+        public void AddProdukt(Produkt neuesProdukt)
+        {
+            //Automatische ChangeNotification weil ObservableCollection
+            ProduktList.Add(neuesProdukt);
+            ProduktAnzahl = ProduktList.Count;
+        }
+
+
+    }
+}
---
+++ HelloCampus02Demo/Views/ProduktListWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using HelloCampus02Demo.Models;
+using HelloCampus02Demo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelloCampus02Demo.Views
+{
+    /// <summary>
+    /// Interaction logic for ProduktListWindow.xaml
+    /// </summary>
+    public partial class ProduktListWindow : Window
+    {
+        public ProduktListWindow()
+        {
+            InitializeComponent();
+
+            ProductListViewModel vm = new ProductListViewModel();
+            vm.AusgewaehltesProdukt = new Produkt()
+            {
+                Bezeichnung = "noch nichts ausgewählt"
+            };
+            vm.ProduktList=new System.Collections.ObjectModel.ObservableCollection<Models.Produkt>();
+
+            vm.AddProdukt(new Models.Produkt 
+            { 
+                ProduktID = 1, Bezeichnung = "Produkt 1", 
+                Kategorie = "Yellow", AufLager = true, Preis = 10.0 
+            });
+            vm.AddProdukt(new Models.Produkt 
+            { 
+                ProduktID = 2, Bezeichnung = "Produkt 2", 
+                Kategorie = "Blue", AufLager = false, Preis = 20.0 
+            });
+
+            this.DataContext = vm;
+
+        }
+
+        private void NeuesProduktHinzufuegen(object sender, RoutedEventArgs e)
+        {
+            Produkt pNeu = new Produkt()
+            {
+                ProduktID = 17,
+                Bezeichnung = "Neues Produkt",
+                Kategorie = "Green"
+            };
+
+            var vm= this.DataContext as ProductListViewModel;
+            if (vm != null)
+            {
+                vm.AddProdukt(pNeu);
+            }
+            
+        }
+    }
+}
